24762dfad9c458606ad4dc2fdf1020389c638644
Almansur342 <abdullahalmansur2002@gmail.com>
2024-03-13 22:40:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl bg-red-50 text-center my-7'>Hello</h1>
+    </>
+  )
+}
+
+export default App
